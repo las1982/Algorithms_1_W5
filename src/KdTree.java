eeb730dae9f24d82baeceaf9d3eaba44837a33aa
las1982 <lashkov_alex@mail.ru>
2017-05-09 22:50:21 +0300

    commit

--- src/KdTree.java
+++ src/KdTree.java
@@ -1,12 +1,10 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Daniel on 20/07/15.
- */
 public class KdTree {
     private Node root;
 
