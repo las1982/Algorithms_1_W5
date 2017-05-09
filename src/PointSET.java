0a70bdc1c41e3804a539219910063c7c0bbc7de3
las1982 <lashkov_alex@mail.ru>
2017-05-09 22:56:08 +0300

    commit

--- src/PointSET.java
+++ src/PointSET.java
@@ -11,42 +11,49 @@ import java.util.TreeSet;
 public class PointSET {
     private Set<Point2D> set;
 
+    // construct an empty set of points
     public PointSET() {
         this.set = new TreeSet<Point2D>();
     }
 
+    // is the set empty?
     public boolean isEmpty() {
         return this.set.isEmpty();
     }
 
+    // number of points in the set
     public int size() {
         return this.set.size();
     }
 
+    // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
         this.set.add(p);
 
     }
 
+    // does the set contain point p?
     public boolean contains(Point2D p) {
         return this.set.contains(p);
     }
 
+    // draw all points to standard draw
     public void draw() {
         for (Point2D p : this.set) {
             StdDraw.point(p.x(), p.y());
         }
     }
 
+    // all points that are inside the rectangle
     public Iterable<Point2D> range(RectHV rect) {
         List<Point2D> ret = new ArrayList<Point2D>();
         for (Point2D p : this.set)
             if (rect.contains(p))
                 ret.add(p);
-
         return ret;
     }
 
+    // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
         Point2D ret = null;
         for (Point2D c : this.set)
